e24eeb236299bfd87cd5545d99261fdaa64a275e
Jungyo Yang(Window) <2jungyo@naver.com>
2025-11-14 18:55:38 +0900

    controller 공부중

--- src/main/java/com/kt/controller/UserController.java
+++ src/main/java/com/kt/controller/UserController.java
@@ -1,18 +1,29 @@
 package com.kt.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.kt.dto.UserCreateRequest;
+import com.kt.service.UserService;
+
+import lombok.RequiredArgsConstructor;
+
 @RestController
-public class UserController {
+@RequiredArgsConstructor
+@RequestMapping("/users")
+public class UserController extends SwaggerAssistance {
+	private final UserService userService;
+
 	@PostMapping("/users")
 	@ResponseStatus(HttpStatus.CREATED)
 	// loginId, password, name, birthday
 	// json형태의 body에 담겨서 postdycjddmfh /users로 들어오면
 	// @RequestBody를 보고 jacksonObjectMapper가 동작해서 json을 읽어서 dto로 변환
-	public void create(@RequestBody UserCreateRequest request){
+
 		//jackson object mapper -> json to dto를 매핑
 
 	}
