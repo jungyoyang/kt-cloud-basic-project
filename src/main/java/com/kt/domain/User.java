6e1aa60743eee6e89c0753d0e3f5ffcd21b3550a
Jungyo Yang(Window) <2jungyo@naver.com>
2025-11-23 02:48:14 +0900

    feat: domain'user' 생성일, 수정일, ID

--- src/main/java/com/kt/domain/User.java
+++ src/main/java/com/kt/domain/User.java
@@ -1,6 +1,7 @@
 package com.kt.domain;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -10,8 +11,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
-	private String loginId;
-	private String password;
-	private String name;
-	private LocalDate birthday;
+	private Long id;                 // DB에서 생성되는 PK
+	private String loginId;          // 로그인 아이디
+	private String password;         // 암호화된 비밀번호
+	private String name;             // 유저 이름
+	private String email;            // 이메일
+	private String mobile;           // 전화번호
+	private Gender gender;           // 성별 (Enum)
+	private LocalDate birthday;      // 생일
+	private LocalDateTime createdAt; // 생성일
+	private LocalDateTime updatedAt; // 수정일
 }
