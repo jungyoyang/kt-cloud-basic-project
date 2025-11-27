4c8190371caf2dab76207fa19562c811dab36afa
Jungyo Yang(Window) <2jungyo@naver.com>
2025-11-27 23:57:24 +0900

    jpa공부 (11.27)

---
+++ src/main/java/com/kt/domain/User.java
@@ -0,0 +1,4 @@
+package com.kt.domain;
+
+public class User {
+}
---
+++ src/main/java/com/kt/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.kt.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.kt.domain.User;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+}
