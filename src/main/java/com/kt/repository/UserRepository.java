8c888d2df34e0a21a4d7a74c0ce111aa58b9d8c2
Jungyo Yang(Window) <2jungyo@naver.com>
2025-11-28 23:55:11 +0900

    jpa공부 (11.28)

--- src/main/java/com/kt/repository/UserRepository.java
+++ src/main/java/com/kt/repository/UserRepository.java
@@ -5,7 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.kt.domain.User;
 
 // <T,ID>
-// T: Entity 클래스
-// ID: Entity 클래스의 PK 타입
+// T: Entity 클래스 => User
+// ID: Entity 클래스의 PK 타입 = Long
 public interface UserRepository extends JpaRepository<User,Long> {
 }
+// 이러면 CRUD기능이 끝!
