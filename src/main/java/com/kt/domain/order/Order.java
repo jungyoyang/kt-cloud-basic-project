e8a5c79f2541dae2cdc3f137d5f2b89aed1ae88a
Jungyo Yang(Window) <2jungyo@naver.com>
2025-12-07 02:10:25 +0900

    ENUM (12.7)

---
+++ src/main/java/com/kt/domain/order/Order.java
@@ -0,0 +1,11 @@
+package com.kt.domain.order;
+
+import com.kt.common.BaseEntity;
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Order extends BaseEntity {
+}
--- src/main/java/com/kt/domain/product/Product.java
+++ src/main/java/com/kt/domain/product/Product.java
@@ -4,6 +4,8 @@ package com.kt.domain.product;
 import com.kt.common.BaseEntity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import lombok.Getter;
 
 @Entity
@@ -13,7 +15,15 @@ public class Product extends BaseEntity {
 	private String name;
 	private int price;
 	private int stock;
+	@Enumerated(EnumType.STRING)
 	private ProductStatus status;
 
+	//생성
+	//수정
+// 	삭제
+// 	조회(리스트,단건)
+// 상태변경
+// 	재고수량감소
+// 	재고수량증가
 
 }
