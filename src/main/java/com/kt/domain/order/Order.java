00293e7a732c1cf1ce65e2bfcc4ddc3caf4e8149
Jungyo Yang(Window) <2jungyo@naver.com>
2025-12-08 23:06:54 +0900

    Order (12.8)

--- src/main/java/com/kt/domain/order/Order.java
+++ src/main/java/com/kt/domain/order/Order.java
@@ -1,8 +1,12 @@
 package com.kt.domain.order;
 
+import java.time.LocalDateTime;
+
 import com.kt.common.BaseEntity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Table;
 import lombok.Getter;
 
@@ -13,6 +17,8 @@ public class Order extends BaseEntity {
 	private String receiverName;
 	private String receiverAddress;
 	private String receiverMobile;
-	private Localdatetime deliveredAt;
+	@Enumerated(EnumType.STRING) //숫자로 DB에저장하는게아닌 STRING으로 저장하기위함
+	private Status status;
+	private LocalDateTime deliveredAt;
 
 }
