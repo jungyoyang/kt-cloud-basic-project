95799a5d38f28e9c5882c0a24b245c45f6d7545e
Jungyo Yang(Window) <2jungyo@naver.com>
2025-12-08 23:59:48 +0900

    Order (12.8)

--- src/main/java/com/kt/domain/order/Order.java
+++ src/main/java/com/kt/domain/order/Order.java
@@ -3,10 +3,12 @@ package com.kt.domain.order;
 import java.time.LocalDateTime;
 
 import com.kt.common.BaseEntity;
+import com.kt.domain.user.User;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 
@@ -18,7 +20,19 @@ public class Order extends BaseEntity {
 	private String receiverAddress;
 	private String receiverMobile;
 	@Enumerated(EnumType.STRING) //숫자로 DB에저장하는게아닌 STRING으로 저장하기위함
-	private Status status;
+	private OrderStatus status;
 	private LocalDateTime deliveredAt;
+
+	//연관관계
+	//주문 <-> 회원
+	// N : 1 => 다대일
+	// ManyToOne
+	// FK => 다쪽, 많은쪽에 자동으로 생김
+
+
+	@ManyToOne
+	private User user;
+	// 하나의 주문은 여러명의 회원을 가질 수 있나? 아니요
+
 
 }
--- src/main/java/com/kt/domain/order/OrderStatus.java
+++ src/main/java/com/kt/domain/order/OrderStatus.java
@@ -15,11 +15,5 @@ public enum OrderStatus {
 
 	private final String description;
 
-	//연관관계
-	//주문 <-> 회원
-	// N : 1 => 다대일
-	// FK => 다쪽, 많은쪽에 자동으로 생김
-
-	// 하나의 주문은 여러명의 회원을 가질 수 있나? 아니요
 
 }
