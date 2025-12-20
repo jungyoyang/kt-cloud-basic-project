2936e0e8d8cfb788eebe20d03db495b464c082c6
Jungyo Yang(Window) <2jungyo@naver.com>
2025-12-20 21:58:47 +0900

    ResoponseEntity(12.20)

---
+++ src/main/java/com/kt/common/ApiAdvice.java
@@ -0,0 +1,19 @@
+package com.kt.common;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ApiAdvice {
+	// 어떤 예외를 처리할 것인지 정의
+	// MethodArgumentNotValidException 이 익셉션을 처리하도록
+	// @ExceptionHandler(MethodArgumentNotValidException.class)
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<String> MethodArgumentNotValidException(MethodArgumentNotValidException e) {
+		//예외처리 로직 작성
+		return ErrorResponse.error(HttpStatus.BAD_REQUEST,"에러임");
+	}
+}
--- src/main/java/com/kt/common/ApiResult.java
+++ src/main/java/com/kt/common/ApiResult.java
@@ -8,16 +8,15 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-//API 응답을 표준화하기 위한 클래스
+// API 응답을 표준화하기 위한 클래스
 // Spring에서는 Http응답을 처리해주는 객체가 존재함
 // ResponseEntity
-
 @Getter
 @AllArgsConstructor
 public class ApiResult<T> {
 	private String code;
 	private String message;
-	@JsonInclude(JsonInclude.Include.NON_NULL)
+	@JsonInclude(JsonInclude.Include.NON_NULL) // data가 null일경우 빼버리자
 	private T data;
 
 	public static ApiResult<Void> ok() {
@@ -29,7 +28,7 @@ public class ApiResult<T> {
 		return ApiResult.of("ok", "성공", data);
 	}
 
-	public static <T> ApiResult<T> of(String code, String message, T data) {
+	private static <T> ApiResult<T> of(String code, String message, T data) {
 		return new ApiResult<>(code, message, data);
 	}
 
