7db4feafece3b2eb7d6e4d427cca4f549b41a22b
Jungyo Yang(Window) <2jungyo@naver.com>
2026-01-11 00:22:25 +0900

    order부분(1.11)

--- src/main/java/com/kt/service/OrderService.java
+++ src/main/java/com/kt/service/OrderService.java
@@ -3,7 +3,10 @@ package com.kt.service;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.kt.common.ErrorCode;
 import com.kt.domain.order.Order;
+import com.kt.domain.order.Receiver;
+import com.kt.repository.ProductRepository;
 import com.kt.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -13,9 +16,21 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class OrderService {
 	private final UserRepository userRepository;
-	//주문생성
-	public void create(Long userId){
+	private final ProductRepository productRepository;
 
+	//주문생성
+	public void create(
+		Long userId,
+		String receiverName,
+		String receiverAddress,
+		String receiverMobile
+	) {
+		var user = userRepository.findByIdOrThrow(userId, ErrorCode.NOT_FOUND_USER);
+		var receiver = new Receiver(
+			receiverName,
+			receiverAddress,
+			receiverMobile
+		);
 		new Order();
 	}
 }
